c0f7e414809ceeeaa491584104a05886efaad9ed
Ivan Munguia <xxthecreatorxx_live@hotmail.com>
2017-02-19 03:54:32 +0000

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.3.1/css/bulma.min.css" type="text/css">
+
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+        <script src="https://unpkg.com/vue@2.1.10/dist/vue.js"></script>
+    </body>
+</html>
